a771576876140b015c476a5d468143f388ebfd89
InPRTx <inprtx@gmail.com>
2024-02-24 19:17:37 +0800

    mod: 添加容器依赖顺序，将update做成驻守进程

--- update.sh
+++ update.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env sh
-ip link add dummydn42 type dummy
-ip addr add 172.20.197.176/32 dev dummydn42
-ip addr add 172.20.197.177/32 dev dummydn42
-ip addr add 172.20.197.178/32 dev dummydn42
-ip addr add 172.20.197.179/32 dev dummydn42
-ip addr add fdf4:56da:a360::/128 dev dummydn42
-ip addr add fdf4:56da:a360::1/128 dev dummydn42
-ip addr add fdf4:56da:a360::2/128 dev dummydn42
-ip link set dummydn42 up
-ip link add dunmmypub type dummy
-ip addr add 23.146.72.2/32 dev dunmmypub
-ip addr add 2a13:b487:4200::/128 dev dunmmypub
-ip addr add 2a13:b487:4200::1/128 dev dunmmypub
-ip addr add 2a13:b487:4200::2/128 dev dunmmypub
-ip addr add 2a13:b487:4200::3/128 dev dunmmypub
-ip link set dunmmypub up
+ip link add dummydn42 type dummy || true
+ip addr add 172.20.197.176/32 dev dummydn42 || true
+ip addr add 172.20.197.177/32 dev dummydn42 || true
+ip addr add 172.20.197.178/32 dev dummydn42 || true
+ip addr add 172.20.197.179/32 dev dummydn42 || true
+ip addr add fdf4:56da:a360::/128 dev dummydn42 || true
+ip addr add fdf4:56da:a360::1/128 dev dummydn42 || true
+ip addr add fdf4:56da:a360::2/128 dev dummydn42 || true
+ip link set dummydn42 up || true
+ip link add dunmmypub type dummy || true
+ip addr add 23.146.72.2/32 dev dunmmypub || true
+ip addr add 2a13:b487:4200::/128 dev dunmmypub || true
+ip addr add 2a13:b487:4200::1/128 dev dunmmypub || true
+ip addr add 2a13:b487:4200::2/128 dev dunmmypub || true
+ip addr add 2a13:b487:4200::3/128 dev dunmmypub || true
+ip link set dunmmypub up || true
 sleep 2
-ip addr del 100.64.0.2/30 dev eth0
+ip addr del 100.64.0.2/30 dev eth0 || true
+while true; do
+    sleep 3600
+done
