14a975c0d1b7224d65f46c4deb6414dc2d49b0a1
InPRTx <inprtx@gmail.com>
2024-02-24 18:52:40 +0800

    fix: 内网不同，延迟2s删除默认ipv4

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-ip addr del 100.64.0.2/30 dev eth0
 ip link add dummydn42 type dummy
 ip addr add 172.20.197.176/32 dev dummydn42
 ip addr add 172.20.197.177/32 dev dummydn42
@@ -16,3 +15,5 @@ ip addr add 2a13:b487:4200::1/128 dev dunmmypub
 ip addr add 2a13:b487:4200::2/128 dev dunmmypub
 ip addr add 2a13:b487:4200::3/128 dev dunmmypub
 ip link set dunmmypub up
+sleep 2
+ip addr del 100.64.0.2/30 dev eth0
