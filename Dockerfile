1a1649acecbe43146b3ecaa060bb5778134f8b53
InPRTx <inprtx@gmail.com>
2024-02-24 16:25:00 +0800

    add: ip 添加默认dummy ip

--- Dockerfile
+++ Dockerfile
@@ -8,3 +8,7 @@ Package: bird2\n\
 Pin: release a=unstable\n\
 Pin-Priority: 600' > /etc/apt/preferences && echo 'deb http://deb.debian.org/debian sid main'>>/etc/apt/sources.list
 RUN apt update && apt install -y iproute2 net-tools iputils-ping bird2 curl mtr-tiny htop tcpdump dnsmasq --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+COPY init.sh /usr/local/bin/init.sh
+COPY bird.conf /etc/bird/bird.conf
+COPY dnsmasq.conf /etc/dnsmasq.conf
---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+ip link add dummydn42 type dummy
+ip addr add 172.20.197.176/32 dev dummydn42
+ip addr add 172.20.197.177/32 dev dummydn42
+ip addr add 172.20.197.178/32 dev dummydn42
+ip addr add 172.20.197.179/32 dev dummydn42
+ip addr add fdf4:56da:a360::/128 dev dummydn42
+ip addr add fdf4:56da:a360::1/128 dev dummydn42
+ip addr add fdf4:56da:a360::2/128 dev dummydn42
+ip link set dummydn42 up
+ip link add dunmmypub type dummy
+ip addr add 23.146.72.2/32 dev dunmmypub
+ip addr add 2a13:b487:4200::/128 dev dunmmypub
+ip addr add 2a13:b487:4200::1/128 dev dunmmypub
+ip addr add 2a13:b487:4200::2/128 dev dunmmypub
+ip addr add 2a13:b487:4200::3/128 dev dunmmypub
+ip link set dunmmypub up
