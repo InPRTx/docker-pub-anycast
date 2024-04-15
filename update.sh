827868a8b51efe860ca67bbb7b36c5c6c350244d
InPRTx <inprtx@gmail.com>
2024-04-15 10:14:13 +0800

    mod: 更新使用ip

--- update.sh
+++ update.sh
@@ -7,14 +7,14 @@ ip addr add fdf4:56da:a360::1/128 dev dummydn42 || true
 ip addr add fdf4:56da:a360::2/128 dev dummydn42 || true
 ip link set dummydn42 up || true
 ip link add dunmmypub type dummy || true
-ip addr add 23.146.72.2/32 dev dunmmypub || true
-ip addr add 2a13:b487:4200::/128 dev dunmmypub || true
-ip addr add 2a13:b487:4200::1/128 dev dunmmypub || true
-ip addr add 2a13:b487:4200::2/128 dev dunmmypub || true
-ip addr add 2a13:b487:4200::3/128 dev dunmmypub || true
+ip addr add 100.64.0.2/32 dev dunmmypub || true
+ip addr add 2a13:a5c3:f100::/128 dev dunmmypub || true
+ip addr add 2a13:a5c3:f100::1/128 dev dunmmypub || true
+ip addr add 2a13:a5c3:f100::2/128 dev dunmmypub || true
+ip addr add 2a13:a5c3:f100::3/128 dev dunmmypub || true
 ip link set dunmmypub up || true
 sleep 2
-ip addr del 100.64.0.2/30 dev eth0 || true
+ip addr del 10.253.169.2/30 dev eth0 || true
 while true; do
     sleep 3600
 done
