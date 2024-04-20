d3e9ab356fe12f99cd081f58bc417b1f1eb873e3
InPRTx <inprtx@gmail.com>
2024-04-20 15:38:08 +0800

    fix: rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-podman stop pub-anycast pub-anycast-bird pub-anycast-update pub-anycast-dnsmasq pub-anycast-nginx|| true
-podman rm pub-anycast pub-anycast-bird pub-anycast-update pub-anycast-dnsmasq pub-anycast-nginx|| true
+podman stop pub-anycast-nginx || true
+podman stop pub-anycast-dnsmasq || true
+podman stop pub-anycast-update || true
+podman stop pub-anycast-bird || true
+podman stop pub-anycast || true
+podman rm pub-anycast-nginx || true
+podman rm pub-anycast-dnsmasq || true
+podman rm pub-anycast-update || true
+podman rm pub-anycast-bird || true
+podman rm pub-anycast || true
 podman network rm podman-pub-anycast_host_bridge || true
 podman-compose up -d --build
