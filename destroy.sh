7026eea5f89d15bcf3707a61b1a183f4ba0fb866
InPRTx <inprtx@gmail.com>
2024-05-10 15:57:36 +0800

    fix: docker-compose.yaml

--- destroy.sh
+++ destroy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+podman stop pub-bird-lg || true
 podman stop pub-anycast-caddy || true
 podman stop pub-anycast-dnsmasq || true
 podman stop pub-anycast-update || true
 podman stop pub-anycast-bird || true
 podman stop pub-anycast || true
+podman rm -f pub-bird-lg || true
 podman rm -f pub-anycast-caddy || true
 podman rm -f pub-anycast-dnsmasq || true
 podman rm -f pub-anycast-update || true
